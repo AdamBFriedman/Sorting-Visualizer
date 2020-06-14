cd04ed824c1ec4d41cff6f9f40713b7e5ca8a316
Adam Friedman <adam@blok.party>
2020-06-13 19:13:27 -0700

    Created base for Visualizer including random num generator

--- src/components/SortingVisualizer.js
+++ src/components/SortingVisualizer.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 
 class SortingVisualizer extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      array: []
+    };
+  }
+
+  componentDidMount() {
+    this.resetArray();
+  }
+
+  resetArray() {
+    const array = [];
+
+    for (let i=0; i< 100; i++) {
+      array.push(randomNumFromInterval(5, 1000));
+    }
+
+    this.setState({array});
+  }
     render(){
+      const {array} = this.state;
         return(
-        <p>
-          Hello Adam
-        </p>
+        <>
+          {array.map((value, idx) => (
+            <div className="array-bar" key={idx}>
+              {value}
+            </div>
+          ))}
+        </>
         )
     }
 } 
+
+function randomNumFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 export default SortingVisualizer;
