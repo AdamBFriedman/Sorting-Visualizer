2767e3001dc5c73512ecad6f2d98573de0a852a3
Adam Friedman <adam@blok.party>
2020-06-13 18:21:57 -0700

    Set up SortingVisualizer component

---
+++ src/components/SortingVisualizer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SortingVisualizer extends Component{
+    render(){
+        return(
+        <p>
+          Hello Adam
+        </p>
+        )
+    }
+} 
+
+export default SortingVisualizer;
