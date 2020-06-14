ce9d7bc3e846ecd65383b84230edba33f9b49c84
Adam Friedman <adam@blok.party>
2020-06-14 13:57:53 -0700

    Added a way for users to adjust size of array

--- src/components/SortingVisualizer.js
+++ src/components/SortingVisualizer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {getMergeSortAnimations} from '../sortingAlgorithms';
+import { getMergeSortAnimations } from '../sortingAlgorithms';
 
 // Change this value for the speed of the animations.
 const ANIMATION_SPEED_MS = 1;
@@ -17,80 +17,106 @@ let STARTING_COLOR = '#ed3330';
 const SECONDARY_COLOR = '#451e3e';
 
 export default class SortingVisualizer extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      array: [],
-    };
-  }
+		this.state = {
+			array: [],
+			input: '',
+		};
+	}
 
-  componentDidMount() {
-    this.resetArray();
-  }
+	componentDidMount() {
+		this.resetArray();
+	}
 
-  resetArray() {
-    const array = [];
-    for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
-      array.push(randomNumFromInterval(5, 730));
-    }
-    this.setState({array});
-  }
-  
-  mergeSort() {
-    const animations = getMergeSortAnimations(this.state.array);
-    for (let i = 0; i < animations.length; i++) {
-      const arrayBars = document.getElementsByClassName('array-bar');
-      const isColorChange = i % 3 !== 2;
-      if (isColorChange) {
-        const [barOneIdx, barTwoIdx] = animations[i];
-        const barOneStyle = arrayBars[barOneIdx].style;
-        const barTwoStyle = arrayBars[barTwoIdx].style;
-        const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
-        setTimeout(() => {
-          barOneStyle.backgroundColor = color;
-          barTwoStyle.backgroundColor = color;
-        }, i * ANIMATION_SPEED_MS);
-      } else {
-        setTimeout(() => {
-          const [barOneIdx, newHeight] = animations[i];
-          const barOneStyle = arrayBars[barOneIdx].style;
-          barOneStyle.height = `${newHeight}px`;
-        }, i * ANIMATION_SPEED_MS);
-      }
-    }
-  }
+	resetArray() {
+		const array = [];
+		for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
+			array.push(randomNumFromInterval(5, 730));
+		}
+		this.setState({ array });
+	}
 
-  randomColor() {
-    let randomColor = "#" + Math.random().toString(16).slice(2, 8)
-    STARTING_COLOR = randomColor;
-    this.resetArray();
-  }
+	mergeSort() {
+		const animations = getMergeSortAnimations(this.state.array);
+		for (let i = 0; i < animations.length; i++) {
+			const arrayBars = document.getElementsByClassName('array-bar');
+			const isColorChange = i % 3 !== 2;
+			if (isColorChange) {
+				const [barOneIdx, barTwoIdx] = animations[i];
+				const barOneStyle = arrayBars[barOneIdx].style;
+				const barTwoStyle = arrayBars[barTwoIdx].style;
+				const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+				setTimeout(() => {
+					barOneStyle.backgroundColor = color;
+					barTwoStyle.backgroundColor = color;
+				}, i * ANIMATION_SPEED_MS);
+			} else {
+				setTimeout(() => {
+					const [barOneIdx, newHeight] = animations[i];
+					const barOneStyle = arrayBars[barOneIdx].style;
+					barOneStyle.height = `${newHeight}px`;
+				}, i * ANIMATION_SPEED_MS);
+			}
+		}
+	}
+
+	newArray() {
+		const array = [];
+		for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
+			array.push(randomNumFromInterval(5, 730));
+		}
+		this.setState({ array });
+	}
+
+	randomColor() {
+		let randomColor = '#' + Math.random().toString(16).slice(2, 8);
+		STARTING_COLOR = randomColor;
+		this.resetArray();
+	}
+
+	handleChange(e) {
+		this.setState({ input: e.target.value });
+	}
+
+	handleClick() {
+		const array = [];
+		for (let i = 0; i < this.state.input; i++) {
+			array.push(randomNumFromInterval(5, 730));
+		}
+		this.setState({ array });
+	}
 
 	render() {
 		const { array } = this.state;
 		return (
-      <>
-			<div className="array-container">
-				{array.map((value, idx) => (
-          <div 
-            className="array-bar" 
-            key={idx}
-						style={{height: `${value}px`, backgroundColor: STARTING_COLOR}}>
-					</div>
-				))}
-        
-			</div>
-      <div class="vintage__container">
-  <p class="vintage vintage__top">MERGE SORT</p>
-  <p class="vintage vintage__bot">MERGE SORT</p>
-</div>
-      <div className="button-row">
-        <button onClick={() => this.resetArray()}>Generate New Array</button>
-        <button onClick={() => this.mergeSort()}>Merge Sort</button>
-        <button onClick={() => this.randomColor()}>Random Color</button>
-      </div>
-      </>
+			<>
+				<div className="array-container">
+					{array.map((value, idx) => (
+						<div
+							className="array-bar"
+							key={idx}
+							style={{ height: `${value}px`, backgroundColor: STARTING_COLOR }}
+						></div>
+					))}
+				</div>
+				<div className="mergeSort__container">
+					<p className="mergeSort mergeSort__top">MERGE SORT</p>
+					<p className="mergeSort mergeSort__bot">MERGE SORT</p>
+				</div>
+				<div className="button-row">
+					<button onClick={() => this.resetArray()}>Generate New Array</button>
+					<button onClick={() => this.mergeSort()}>Merge Sort</button>
+					<button onClick={() => this.randomColor()}>Random Color</button>
+
+					<label>
+						What length would you like the array (enter a number)?
+					</label>
+					<input type="text" name="input" value={this.state.input} onChange={this.handleChange.bind(this)} />
+					<button type="button" onClick={() => this.handleClick()}>Enter</button>
+				</div>
+			</>
 		);
 	}
 }
@@ -98,5 +124,3 @@ export default class SortingVisualizer extends React.Component {
 function randomNumFromInterval(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
-
