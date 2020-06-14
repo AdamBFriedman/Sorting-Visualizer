45468c4ed7fe050c4544f95e3cebde7760eaee05
Adam Friedman <adam@blok.party>
2020-06-13 20:30:22 -0700

    Added some styling to show the data better

--- src/components/SortingVisualizer.js
+++ src/components/SortingVisualizer.js
@@ -1,43 +1,45 @@
 import React, { Component } from 'react';
 
-class SortingVisualizer extends Component{
-  constructor(props) {
-    super(props);
+class SortingVisualizer extends Component {
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      array: []
-    };
-  }
+		this.state = {
+			array: [],
+		};
+	}
 
-  componentDidMount() {
-    this.resetArray();
-  }
+	componentDidMount() {
+		this.resetArray();
+	}
 
-  resetArray() {
-    const array = [];
+	resetArray() {
+		const array = [];
 
-    for (let i=0; i< 100; i++) {
-      array.push(randomNumFromInterval(5, 1000));
-    }
+		for (let i = 0; i < 310; i++) {
+			array.push(randomNumFromInterval(5, 730));
+		}
 
-    this.setState({array});
-  }
-    render(){
-      const {array} = this.state;
-        return(
-        <>
-          {array.map((value, idx) => (
-            <div className="array-bar" key={idx}>
-              {value}
-            </div>
-          ))}
-        </>
-        )
-    }
-} 
+		this.setState({ array });
+	}
+	render() {
+		const { array } = this.state;
+		return (
+			<div className="array-container">
+				{array.map((value, idx) => (
+          <div 
+            className="array-bar" 
+            key={idx}
+						style={{height: `${value}px`}}>
+					</div>
+				))}
+			</div>
+		);
+	}
+}
 
 function randomNumFromInterval(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 export default SortingVisualizer;
