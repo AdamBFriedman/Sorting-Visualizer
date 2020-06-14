3a536401fba05c10a8a9e8402754a7ebc3fa90d4
Adam Friedman <adam@blok.party>
2020-06-13 21:47:30 -0700

    Finished merge sort

--- src/components/SortingVisualizer.js
+++ src/components/SortingVisualizer.js
@@ -1,30 +1,61 @@
 import React, { Component } from 'react';
+import {getMergeSortAnimations} from '../sortingAlgorithms';
 
-class SortingVisualizer extends Component {
-	constructor(props) {
-		super(props);
+// Change this value for the speed of the animations.
+const ANIMATION_SPEED_MS = 1;
 
-		this.state = {
-			array: [],
-		};
-	}
+// Change this value for the number of bars (value) in the array.
+const NUMBER_OF_ARRAY_BARS = 310;
 
-	componentDidMount() {
-		this.resetArray();
-	}
+// This is the main color of the array bars.
+const PRIMARY_COLOR = '#234432';
 
-	resetArray() {
-		const array = [];
+// This is the color of array bars that are being compared throughout the animations.
+const SECONDARY_COLOR = 'red';
 
-		for (let i = 0; i < 550; i++) {
-			array.push(randomNumFromInterval(5, 1000));
-		}
+export default class SortingVisualizer extends React.Component {
+  constructor(props) {
+    super(props);
 
-		this.setState({ array });
+    this.state = {
+      array: [],
+    };
+  }
+
+  componentDidMount() {
+    this.resetArray();
+  }
+
+  resetArray() {
+    const array = [];
+    for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
+      array.push(randomNumFromInterval(5, 730));
+    }
+    this.setState({array});
   }
   
   mergeSort() {
-
+    const animations = getMergeSortAnimations(this.state.array);
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName('array-bar');
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * ANIMATION_SPEED_MS);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i * ANIMATION_SPEED_MS);
+      }
+    }
   }
 
   quickSort() {
@@ -69,4 +100,4 @@ function randomNumFromInterval(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-export default SortingVisualizer;
+
